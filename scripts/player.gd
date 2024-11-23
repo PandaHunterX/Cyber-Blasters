a76add1d85d8492f21361b68cda5bead1311ad38
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2024-11-23 14:49:40 +0800

    Player/Enemy Bug Fix
    Player cannot move when dead, and the enemy will follow the player and attack when it enters the hitbox. Both have their own health

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,35 +7,49 @@ extends CharacterBody2D
 var notmoving = true
 var is_hit = false
 var health = 1000
+var horizontal_direction = null
+var isdead = false
+
+func _ready() -> void:
+	$meleehitbox/CollisionShape2D.disabled = true
 		
 func _physics_process(delta: float) -> void:
-	var horizontal_direction = Input.get_axis("move_right", "move_left")
-	velocity.x = 300 * horizontal_direction
-	move_and_slide()
-	update_animations(horizontal_direction)
+	if not isdead:
+		var horizontal_direction = Input.get_axis("move_right", "move_left")
+		velocity.x = 300 * horizontal_direction
+		move_and_slide()
+		update_animations(horizontal_direction)
 	
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack") and notmoving:
+	if event.is_action_pressed("attack") and notmoving and not isdead:
 		ap.play("attack")
+		$AnimationCooldown.start()
 	
 func update_animations(horizontal_direction):
-	if horizontal_direction == 0:
-		if notmoving == false:
-			ap.play("idle")
-			notmoving = true
-	else:
-		if notmoving == true:
-			ap.play("run")
-			notmoving = false
+	if not isdead:
+		if horizontal_direction == 0:
+			if notmoving == false:
+				ap.play("idle")
+				notmoving = true
+		else:
+			if notmoving == true:
+				ap.play("run")
+				notmoving = false
 
 func weak_hit():
 	health -= 100
 	is_hit = true
 	print(health)
 	ap.play("damaged")
+	$AnimationCooldown.start()
 	player_health()
 	
 func  player_health():
 	if health <= 0:
 		ap.play("death")
+		isdead = true
 		
+
+func _on_animation_cooldown_timeout() -> void:
+	update_animations(horizontal_direction)
+	$AnimationCooldown.stop() # Replace with function body.
