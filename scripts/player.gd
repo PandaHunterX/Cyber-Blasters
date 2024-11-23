e9c0bd15d45329ea0847e1b42753b5f726cec923
HiroS404 <rizalbalase@gmail.com>
2024-11-23 11:54:34 +0800

    flip char

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
-
+const JUMP_VELOCITY = -400.0
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 
 func _physics_process(delta):
 		
-	var horizontal_direction = Input.get_axis("move_right", "move_left")
+	var horizontal_direction = Input.get_axis("left", "right")
 	velocity.x = 300 * horizontal_direction
+	
+
+		
 	move_and_slide()
 	update_animations(horizontal_direction)
+	
+	var isLeft = velocity.x < 0
+	sprite.flip_h = isLeft
+	
+	
 	
 func update_animations(horizontal_direction):
 	if horizontal_direction == 0:
 		ap.play("idle")
 	else:
 		ap.play("run") 
+		
+
 	
