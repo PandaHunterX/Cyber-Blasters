900aacf2e61df64343566dd933581ddf7b3b5723
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2024-11-26 23:26:33 +0800

    Camera and RNG System update
    Camera Restriction(like Super Mario), Distance Tracker, RNG for bullet drop rate and enemy spawn, Enemy increases in size and their spawn cooldown decreases every 30 seconds

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,19 +4,28 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 @onready var melee = $meleehitbox
 @onready var weapon = find_child("Weapon")
+@onready var camera = $PlayerCamera
+
+var camera_left_padding = 300
 
 var notmoving = true
 var is_hit = false
 var horizontal_direction = null
-var isdead = false
 var weapon_equip = false
+var previous_position: float
 
 func _ready() -> void:
 	$meleehitbox/CollisionShape2D.disabled = true
 	weapon.texture = null
-		
+	previous_position = global_position.x
+	
 func _physics_process(delta: float) -> void:
-	if not isdead:
+	if (position.x - camera_left_padding) > camera.limit_left:
+		camera.limit_left = position.x - camera_left_padding
+	if not Global.player_isdead:
+		if not velocity.x < 0:
+			previous_position = Global.distance_calculation(global_position.x, previous_position, delta) #Distance Calculatiom
+		#Movement
 		horizontal_direction = Input.get_axis("left", "right")
 		velocity.x = (Global.player_speed * Global.speed_buff) * horizontal_direction
 		var isLeft = velocity.x < 0
@@ -26,21 +35,20 @@ func _physics_process(delta: float) -> void:
 		update_animations(horizontal_direction)
 	
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack") and notmoving and not isdead and not weapon_equip:
+	if event.is_action_pressed("attack") and notmoving and not Global.player_isdead and not weapon_equip:
 		ap.play("attack")
-		$AnimationCooldown.start()
 		await ap.animation_finished
 		notmoving = false
 	elif event.is_action_pressed("pistol") or event.is_action_pressed("smg") or event.is_action_pressed("laser_rifle") or event.is_action_pressed("grenade_launcher"):
 		weapon_equip = true
 	elif event.is_action_pressed("melee"):
 		weapon_equip = false
-	elif event.is_action_pressed("power_up") and Global.powerup_refill == 100:
+	elif event.is_action_pressed("power_up") and Global.powerup_refill >= 100:
 		Global.powerup()
 		$PowerupDuration.start()
 		
 func update_animations(horizontal_direction):
-	if not isdead:
+	if not Global.player_isdead:
 		if horizontal_direction == 0:
 			if notmoving == false:
 				ap.play("idle")
@@ -51,22 +59,19 @@ func update_animations(horizontal_direction):
 				notmoving = false
 
 func weak_hit():
-	Global.player_health -= 10
+	Global.player_health -= 5
 	is_hit = true
 	ap.play("damaged")
-	$AnimationCooldown.start()
+	await  ap.animation_finished
 	player_health()
+	is_hit = false
+	update_animations(horizontal_direction)
 	
 func  player_health():
 	if Global.player_health <= 0:
 		ap.play("death")
-		isdead = true
+		Global.player_isdead = true
 		weapon.texture = null
-
-func _on_animation_cooldown_timeout() -> void:
-	is_hit = false
-	update_animations(horizontal_direction)
-	$AnimationCooldown.stop() 
 
 
 func _on_meleehitbox_body_entered(body: Node2D) -> void:
